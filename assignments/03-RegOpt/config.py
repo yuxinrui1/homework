0aad580e84736dd22aca050bf190750051d3a6e3
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-02-19 13:49:46 -0500

    try

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,13 +8,15 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
     batch_size = 64
     num_epochs = 10
-    initial_learning_rate = 0.001
-    initial_weight_decay = 0.001
-    gamma = 0.1
+    initial_learning_rate = 0.01
+    initial_weight_decay = 0.000
+    T_max = 10
+
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "gamma": gamma,
+        "T_max": 10,
+        "eta_min": 0.001,
     }
 
     optimizer_factory: Callable[
@@ -25,8 +27,4 @@ class CONFIG:
         weight_decay=CONFIG.initial_weight_decay,
     )
 
-    transforms = Compose(
-        [
-            ToTensor(),
-        ]
-    )
+    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
+import math
 import torch
 
 
@@ -9,12 +10,7 @@ class CustomLRScheduler(_LRScheduler):
     custom lr
     """
 
-    def __init__(
-        self,
-        optimizer,
-        last_epoch=-1,
-        gamma=0.1,
-    ):
+    def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1, verbose=False):
         """
         Create a new scheduler.
 
@@ -23,7 +19,8 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        self.gamma = gamma
+        self.T_max = T_max
+        self.eta_min = eta_min
 
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
@@ -40,4 +37,24 @@ class CustomLRScheduler(_LRScheduler):
 
         if self.last_epoch == 0:
             return [group["lr"] for group in self.optimizer.param_groups]
-        return [group["lr"] * self.gamma for group in self.optimizer.param_groups]
+        elif self._step_count == 1 and self.last_epoch > 0:
+            return [
+                self.eta_min
+                + (base_lr - self.eta_min)
+                * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
+                / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
+            return [
+                group["lr"]
+                + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        return [
+            (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+            / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
+            * (group["lr"] - self.eta_min)
+            + self.eta_min
+            for group in self.optimizer.param_groups
+        ]
