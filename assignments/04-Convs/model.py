4ae0fe3f8327f64730ab63a392ca2442342dd195
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-17 17:47:33 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -14,28 +14,33 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
 
-        # self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3, stride =1,padding=1)
-        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        # self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.fc1 = nn.Linear(16 * 8 * 8, 64)
-        # self.fc2 = nn.Linear(32*32*32, num_classes)
-        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(8)
         self.relu1 = nn.ReLU(inplace=True)
-        self.fc1 = nn.Linear(16 * 32 * 32, num_classes)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(8)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.fc = nn.Linear(8 * 8 * 8, num_classes)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         """
-        Performs the forward pass of the model.
+        Defines the forward pass of the model.
 
         Args:
-            x (torch.Tensor): The input tensor with shape (batch_size, num_channels, height, width).
+            x (torch.Tensor): The input tensor of shape (batch_size, 3, 32, 32).
 
         Returns:
-            torch.Tensor: The output tensor with shape (batch_size, num_classes).
+            torch.Tensor: The output tensor of shape (batch_size, num_classes).
         """
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu1(x)
-        x = x.view(-1, 32 * 32 * 32)
-        x = self.fc1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        x = x.view(-1, 8 * 8 * 8)
+        x = self.fc(x)
         return x
