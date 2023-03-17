72e8eeb5240a34da9c479a9b5f0d8fa6040b0196
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-17 17:01:20 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -6,16 +6,27 @@ import torch.nn.functional as F
 class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
+
         self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(32 * 8 * 8, num_classes)
+        self.fc1 = nn.Linear(32 * 8 * 8, 64)
+        self.fc2 = nn.Linear(64, num_classes)
 
-    def forward(self, x):
-        # define the forward pass of the model
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs the forward pass of the model.
+
+        Args:
+            x (torch.Tensor): The input tensor with shape (batch_size, num_channels, height, width).
+
+        Returns:
+            torch.Tensor: The output tensor with shape (batch_size, num_classes).
+        """
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = x.view(-1, 32 * 8 * 8)
-        x = self.fc(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
