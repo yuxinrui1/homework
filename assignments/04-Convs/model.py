cae697d8f90d699bb786820b8566ad4399c211d3
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-17 17:23:17 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -14,11 +14,11 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(32 * 8 * 8, 64)
+        self.fc1 = nn.Linear(16 * 8 * 8, 64)
         self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -33,7 +33,7 @@ class Model(torch.nn.Module):
         """
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 8 * 8)
+        x = x.view(-1, 16 * 8 * 8)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
