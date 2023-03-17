29a211171440f357c4fc85226ccd31fb3422bd95
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-17 17:32:03 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -14,12 +14,15 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, padding=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(16 * 8 * 8, 64)
-        self.fc2 = nn.Linear(64, num_classes)
+        # self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3, stride =1,padding=1)
+        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        # self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # self.fc1 = nn.Linear(16 * 8 * 8, 64)
+        # self.fc2 = nn.Linear(32*32*32, num_classes)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.fc1 = nn.Linear(32 * 32 * 32, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -31,9 +34,8 @@ class Model(torch.nn.Module):
         Returns:
             torch.Tensor: The output tensor with shape (batch_size, num_classes).
         """
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = x.view(-1, 32 * 32 * 32)
+        x = self.fc1(x)
         return x
