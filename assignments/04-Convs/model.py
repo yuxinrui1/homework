4f02c9db719b70c5a7b5f0f4335f06c9d1a3ae2a
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-17 18:23:42 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -25,11 +25,11 @@ class Model(torch.nn.Module):
         self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu1 = nn.ReLU(inplace=True)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+      #  self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(16)
         self.relu2 = nn.ReLU(inplace=True)
-        self.fc = nn.Linear(16 * 8 * 8, num_classes)
+        self.fc = nn.Linear(16 * 32 * 32, num_classes)
 
     def forward(self, x) -> torch.Tensor:
         """
@@ -44,11 +44,11 @@ class Model(torch.nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu1(x)
-        x = self.pool(x)
+       # x = self.pool(x)
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu2(x)
-        x = self.pool(x)
-        x = x.view(-1, 16 * 8 * 8)
+       # x = self.pool(x)
+        x = x.view(-1, 16 * 32 * 32)
         x = self.fc(x)
         return x
