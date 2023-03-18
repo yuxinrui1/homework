08774336d17cccf17a3847470111fb95c1b6cb18
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-03-18 00:54:13 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -22,7 +22,7 @@ class Model(nn.Module):
         """
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=4, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu1 = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -44,11 +44,11 @@ class Model(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu1(x)
+        #x = self.pool(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
         x = self.pool(x)
-        # x = self.conv2(x)
-        # x = self.bn2(x)
-        # x = self.relu2(x)
-        # x = self.pool(x)
         x = x.view(-1, 16 * 16 * 16)
         x = self.fc(x)
         return x
