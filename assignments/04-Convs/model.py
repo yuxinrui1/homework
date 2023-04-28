d8502a12ad7db6802553ee18bef1733a28680146
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-04-28 00:22:39 -0400

    hello

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
-
+import keras
+from keras.datasets import cifar10
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 # import torch.nn.functional as F
 
 
@@ -22,14 +26,14 @@ class Model(nn.Module):
         """
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(8)
         self.relu1 = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 8, kernel_size=1, stride=1, padding=0)
-        self.bn2 = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(16)
         self.relu2 = nn.ReLU(inplace=True)
-        self.fc = nn.Linear(8 * 16 * 16, num_classes)
+        self.fc = nn.Linear(16 * 16 * 16, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -41,14 +45,24 @@ class Model(nn.Module):
         Returns:
             torch.Tensor: The output tensor of shape (batch_size, num_classes).
         """
-        x = self.conv1(x)
+        # Load the pre-trained ResNet-50 model
+        model = ResNet50(weights='imagenet', include_top=True)
+
+        # Make predictions on the test set
+        preds = model.predict(x_test)
+
+        # Convert predictions to class labels
+        pred_labels = np.argmax(preds, axis=1)
+
+        # x = self.conv1(x)
         # x = self.bn1(x)
         # x = self.relu1(x)
         # x = self.pool(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.pool(x)
-        x = x.view(-1, 8 * 16 * 16)
-        x = self.fc(x)
+        #x = self.conv2(x)
+        #x = self.bn2(x)
+        #x = self.relu2(x)
+        #x = self.pool(x)
+        #x = x.view(-1, 16 * 16 * 16)
+        #x = self.fc(x)
+        x = pred_labels
         return x
--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -1,15 +1,32 @@
-import gymnasium as gym
+import gym
+import numpy as np
 
 
 class Agent:
     def __init__(
-        self, action_space: gym.spaces.Discrete, observation_space: gym.spaces.Box
+        self,
+        action_space: gym.spaces.Discrete,
+        observation_space: gym.spaces.Box,
+        alpha: float = 0.1,
+        gamma: float = 0.99,
+        epsilon: float = 1.0,
+        epsilon_min: float = 0.01,
+        epsilon_decay: float = 0.995,
     ):
         self.action_space = action_space
         self.observation_space = observation_space
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.q_table = np.zeros((observation_space.shape[0], action_space.n))
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
-        return self.action_space.sample()
+        if np.random.uniform() < self.epsilon:
+            return self.action_space.sample()
+        else:
+            return np.argmax(self.q_table[observation, :])
 
     def learn(
         self,
@@ -18,4 +35,11 @@ class Agent:
         terminated: bool,
         truncated: bool,
     ) -> None:
-        pass
+        action = self.act(observation)
+        next_q = np.max(self.q_table[observation, :])
+        self.q_table[observation, action] += self.alpha * (
+            reward + self.gamma * next_q - self.q_table[observation, action]
+        )
+        if terminated or truncated:
+            self.epsilon = max(self.epsilon_min, self.epsilon_decay * self.epsilon)
+            self.q_table[observation, action] = reward
