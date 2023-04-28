0365a3f269ea868dcc5a3b399878b62d135c3ad3
yuxinrui1 <yuxinrui@sas.upenn.edu>
2023-04-28 01:40:48 -0400

    hello

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -3,69 +3,66 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-from typing import List
+from typing import List, Tuple
 
 
-# Define Q network
 class QNet(nn.Module):
-    "Docstring"
-
-    def __init__(self, input_size, output_size):
+    "docstring"
+    def __init__(self, state_dim: int, action_dim: int, hidden_dims: List[int]):
         super(QNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, 32)
-        self.fc2 = nn.Linear(32, output_size)
+        dims = [state_dim] + hidden_dims + [action_dim]
+        self.layers = nn.ModuleList()
+        for i in range(len(dims) - 1):
+            self.layers.append(nn.Linear(dims[i], dims[i + 1]))
 
-    def forward(self, x):
-        x = nn.functional.relu(self.fc1(x))
-        x = self.fc2(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers[:-1]:
+            x = nn.functional.relu(layer(x))
+        x = self.layers[-1](x)
         return x
 
 
-# Define Q-learning agent
 class Agent:
-    "Docstring"
-
+    "docstring"
     def __init__(
         self,
-        state_dim,
-        action_dim,
-        lr=0.01,
-        gamma=0.5,
-        epsilon=0.6,
-        epsilon_decay=0.95,
-        min_epsilon=0.1,
+        state_dim: int,
+        action_dim: int,
+        hidden_dims: List[int] = [128, 128],
+        gamma: float = 0.99,
+        epsilon: float = 1.0,
+        epsilon_decay: float = 0.9999,
+        min_epsilon: float = 0.01,
+        learning_rate: float = 1e-3,
+        device: str = "cpu",
     ):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.q_net = QNet(state_dim, action_dim).to(self.device)
-        self.q_target_net = QNet(state_dim, action_dim).to(self.device)
+        self.q_net = QNet(state_dim, action_dim, hidden_dims).to(device)
+        self.q_target_net = QNet(state_dim, action_dim, hidden_dims).to(device)
         self.q_target_net.load_state_dict(self.q_net.state_dict())
-        self.q_target_net.eval()
-        self.optimizer = optim.Adam(self.q_net.parameters(), lr=lr)
+        self.optimizer = optim.Adam(self.q_net.parameters(), lr=learning_rate)
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
         self.min_epsilon = min_epsilon
-        self.action_dim = action_dim
+        self.device = device
 
-    def act(self, state: gym.spaces.Box) -> gym.spaces.Discrete:
-        "Docstring"
-        if np.random.rand() < self.epsilon:
-            return np.random.choice(self.action_dim)
-        else:
-            with torch.no_grad():
-                state = torch.FloatTensor(state).to(self.device)
-                q_values = self.q_net(state)
-                return q_values.argmax().item()
+    def act(self, state: gym.spaces.Box) -> int:
+        "docstring"
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            q_values = self.q_net(state)
+        action = q_values.argmax().item()
+        return action
 
     def learn(
         self,
         state: gym.spaces.Box,
         action: int,
         reward: float,
-        next_state: List[float],
-        done: int,
+        next_state: np.ndarray,
+        done: bool,
     ) -> None:
-        "Docstring"
+        "docstring"
         state = torch.FloatTensor(state).to(self.device)
         action = torch.LongTensor([action]).to(self.device)
         reward = torch.FloatTensor([reward]).to(self.device)
@@ -89,8 +86,10 @@ class Agent:
             self.epsilon *= self.epsilon_decay
 
     def update_target_network(self) -> None:
-        "Docstring"
-        for target_param, param in zip(
-            self.q_target_net.parameters(), self.q_net.parameters()
+        "docstring"
+        for param, target_param in zip(
+            self.q_net.parameters(), self.q_target_net.parameters()
         ):
-            target_param.data.copy_(param.data * 0.01 + target_param.data * 0.99)
+            target_param.data.copy_(
+                self.gamma * target_param.data + (1 - self.gamma) * param.data
+            )
